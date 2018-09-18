bce7bf79c7d15b7f259df1aa43c2b1842c6a2419
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-09-18 20:36:05 +0200

    Xivatos PHP amb echo

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php
+
+$greeting='Hola mon';
+
+// XIVATOS Ctrl + / (teclat numeric)
+//echo "Hola mon"; // Hardcoded Toy example
+echo $greeting;
+
+//var_dump('Hola mon');
+//- Xivatos: echo, dd, dump, Laravel dump server, logs, Laravel DebugBar, var_dump, <pre>
+
+
+// TEST -> TDD
