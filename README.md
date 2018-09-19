81dca55bb0780e07106c8e1450c3409026a759c8
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-09-19 17:58:10 +0200

    HTTP GET per fer pàgines dinàmiques

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # php-learning3
 
 SEPARATION OF CONCERNS
+
+
+HTTP-> HyperText Transfer Protocol <- Xarxes
+
+
+HTTP -> Entre softwares
+
+CLIENT (BROWSER/NAVEGADOR) (html+css+javascript) -> SERVIDOR (php -S Apache/Nginx) (.php)
+                 
+PROTOCOL MISSATGES (2 missatges) 
+                 
+- HTTP REQUEST -> client->servidor
+- HTTP RESPONSE -> servidor->client (PUSH)
+
+HTTP REQUEST (TIPUS/METHODS)
+
+- GET -> DEFECTE
+- POST
+- DELETE
+- PUT
--- index.php
+++ index.php
@@ -1,7 +1,42 @@
 <?php
 
 require 'functions.php';
+//
+//var_dump($_GET['greeting']);
+//die();
 
-$greeting= hello('Mon');
+//$_GET -> array
+//dd($asdasd)
+
+//String -> $greeting='Hola';
+//Booleans -> $x = true/false;
+//Enters -> $num = 15;
+
+//Array/Vector
+
+//$fruites = array();
+//$fruites = [];
+//
+// Normal array
+//$platan = 'platan';
+//$presec = 'prèsec';
+//$taronja = 'taronja';
+//$fruites = [$platan, $presec, $$taronja] ;
+//
+//echo $fruites[0];
+//die()
+
+// Array associatiu
+
+//$person="Sergi Tur Badenas";
+
+//$person = [
+//    'name' => 'Sergi Tur Badenas',
+//    'dni' => '14268078K'
+//    'mobile' => '67845712'
+//    //'email'
+//]
+
+$greeting= hello($_GET['greeting']);
 
 require 'views/index.blade.php';
