b8dd35e2c60721aa30e4b4f633912100e6353bbd
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-09-20 21:30:22 +0200

    Sync

--- tasks.php
+++ tasks.php
@@ -2,16 +2,7 @@
 
 require 'models/Task.php';
 
-//require 'models/Task.php';
 
-////new PDO()
-///
-
-//$task1= [ 'name' => 'Comprar pa', 'completed' => false ];
-//$task2= [ 'name' => 'Comprar llet', 'completed' => false ];
-//$task3= [ 'name' => 'fer llit pa', 'completed' => true ];
-//
-//$tasks = [$task1, $task2, $task3];
 
 $tasks = [
     new Task('Comprar pa', false),
